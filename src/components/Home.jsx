df0850d6c96828309ad058e1e11351a8b24a8a82
milemik <milemik68@gmail.com>
2024-12-21 23:51:33 +0100

    Updated logo and home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,12 @@
-import reactImage from "../assets/react.svg"
+import boxlogo from "../assets/box-logo.png"
 
 export default function Home() {
   return (
     <section id="home" className="grid justify-center p-4">
         <h1 className="font-bold text-4xl color-cyan-400">Welcome to my website</h1>
         <div className="grid justify-center p-4">
-          <img src={reactImage} alt="Test" className="w-72"/>
+          <p className="text-2xl text-center font-bold">Lets box</p>
+          <img src={boxlogo} alt="Test" className="w-72"/>
         </div>
     </section>
   )
